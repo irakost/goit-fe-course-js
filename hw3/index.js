8d1d4c3255f428794a26be643b46c2301bea030e
kostenko iryna <irakost@bigmir.net>
2019-06-19 13:25:56 +0300

    zminu

--- hw3/index.js
+++ hw3/index.js
@@ -1,24 +1,13 @@
 'use strict';
 
-const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
-
-
+const allLogins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 const login = prompt('введите логин');
-
 const isLoginValid = function(login) {
     return login.length >= 4 && login.length <= 16;
   };
   
   const isLoginUnique = function(allLogins, login) {
-    for (let loginItem of logins){
-        if (loginItem === login) {
-
-    if (logins.includes(login)) {
-            return false
-        }
-    }
-
-    return true
+    return !allLogins.includes(login);
   };
 
 
@@ -26,8 +15,8 @@ const isLoginValid = function(login) {
   
   const addLogin = function(allLogins, login) {
    if (isLoginValid(login)){
-    if(isLoginUnique(logins, login)){
-        logins.push(login);
+    if(isLoginUnique(allLogins, login)){
+        allLogins.push(login);
         console.log('Логин успешно добавлен!');
     } else {
         console.log('Такой логин уже используется!');
@@ -36,8 +25,8 @@ const isLoginValid = function(login) {
     console.log('Ошибка! Логин должен быть от 4 до 16 символов');
 }
    };
-   addLogin(logins, login);
- console.log(logins);
+   addLogin(allLogins, login);
+ console.log(allLogins);
 
 
 
